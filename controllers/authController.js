242a5d82f6159de4005715c809e9af11fe22d2ce
Digvijay <dypatil2007@gmail.com>
2026-06-20 00:08:54 +0530

    Added Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,22 @@
-const registerUser = (req, res) => {
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            message: "Please provide all fields"
+        });
+    }
+
     res.json({
-        message: "User registered successfully"
+        name,
+        email,
+        password
     });
-}   
+};
+
 module.exports = {
     registerUser
 };
