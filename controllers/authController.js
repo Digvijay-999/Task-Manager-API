a9402f90881db138158acebfeb48168c1a747c12
Digvijay <dypatil2007@gmail.com>
2026-06-20 00:41:35 +0530

    Check added for duplicate users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 const User = require("../models/User");
 
+
+
+
 const registerUser = async (req, res) => {
 
     const { name, email, password } = req.body;
@@ -9,12 +12,28 @@ const registerUser = async (req, res) => {
             message: "Please provide all fields"
         });
     }
-
-    res.json({
-        name,
-        email,
-        password
+    const existingUser = await User.findOne({
+    email
     });
+    console.log("Existing User:", existingUser);
+   
+    if (existingUser) {
+        return res.status(400).json({
+            message: "User already exists"
+        });
+    }
+    
+    const user = await User.create({
+    name,
+    email,
+    password
+});
+
+res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email
+});
 };
 
 module.exports = {
