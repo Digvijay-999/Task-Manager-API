fa4afb07eb4ca05ad113532b5662c65c8ce78f81
Digvijay <dypatil2007@gmail.com>
2026-06-21 00:28:28 +0530

    successfully added login validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,10 +39,34 @@ res.status(201).json({
     email: user.email
 });
 };
+
 const loginUser = async (req, res) => {
 
+    const { email, password } = req.body;
+    if(!email || !password) {
+        return res.status(400).json({
+            message: "Please provide all fields"
+        });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).json({
+        message: "User not found"
+    });
+}
+    const isMatch = await bcrypt.compare(
+    password,
+    user.password
+);
+    if (!isMatch) {
+        return res.status(400).json({
+            message: "Invalid credentials"
+        });
+    }
+    
     res.json({
-        message: "Login endpoint"
+        email,
+        password
     });
 
 };
